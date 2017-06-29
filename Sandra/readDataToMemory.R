888f080a132899cbf945ef634528e41c6e55510d
System User <sandra.walsh34@gmail.com>
2017-06-29 09:11:39 +0200

    finishes day 1 activities

--- Sandra/readDataToMemory.R
+++ Sandra/readDataToMemory.R
@@ -4,8 +4,12 @@ library(sparklyr)
 library(RSQLite)
 library(dplyr)
 
-
-sc <- spark_connect(master = "local")
+config <- spark_config()
+config$`sparklyr.shell.driver-memory` <- "4G"
+config$`sparklyr.shell.executor-memory` <- "4G"
+config$`spark.yarn.executor.memoryOverhead` <- "1G"
+sc <- spark_connect(master = "local", config = config)
+#sc <- spark_connect(master = "local")
 
 order_products__prior <- NULL
 order_products__train <- NULL
@@ -17,7 +21,6 @@ readInstacart <- function() {
     spark_read_csv(sc, "order_products__prior_tbl", 
                    file.path(DATA_DIR, "order_products__prior.csv"))
   
-<<<<<<< HEAD
   order_products__train <<- 
     spark_read_csv(sc, "order_products__train_tbl", 
                    file.path(DATA_DIR, "order_products__train.csv"))
@@ -25,28 +28,22 @@ readInstacart <- function() {
   orders <<- spark_read_csv(sc, "orders_tbl", file.path(DATA_DIR, "orders.csv"))
   
   products <<- spark_read_csv(sc, "products_tbl", file.path(DATA_DIR, "products.csv"))
-=======
-  #order_products__train <<- 
-   # spark_read_csv(sc, "order_products__train_tbl", 
-    #               file.path(DATA_DIR, "order_products__train.csv"))
-  
-  orders <<- spark_read_csv(sc, "orders_tbl", file.path(DATA_DIR, "orders.csv"))
-  
-  #products <<- spark_read_csv(sc, "products_tbl", file.path(DATA_DIR, "products.csv"))
->>>>>>> d83f7dce0d7297ec3a62b36575db71b23423dd86
 }
 
 
 players <- NULL
+sql_con <- NULL
 
 readFootball <- function() {
-  con <- dbConnect(
+  sql_con <<- dbConnect(
     SQLite(), 
     dbname=file.path(DATA_DIR, "database.sqlite")
   )
-  players <<- tbl_df(dbGetQuery(con,"SELECT * FROM Player"))
+  players <<- tbl_df(dbGetQuery(sql_con,"SELECT * FROM Player"))
+  countries <<- tbl_df(dbGetQuery(sql_con,"SELECT * FROM Country"))
+  games <<- tbl_df(dbGetQuery(sql_con,"SELECT * FROM Match"))
   
-  dbDisconnect(con)
+  #dbDisconnect(sql_con)
 }
 
 
